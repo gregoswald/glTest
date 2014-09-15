0f4043b21cfc5dcb3d87a58c9c9d5b4c8cea11a3
Greg Oswald <oswald.gm@gmail.com>
2014-09-15 14:30:29 +1200

    got a gradiant rendering

--- src/render/GlTest.java
+++ src/render/GlTest.java
@@ -3,10 +3,8 @@ package render;
 public class GlTest {
 	public static void main(String[] args) {
 		renderer render = new renderer();
-		render.initDisplay();
-		render.intitShape();
-		render.intitShaders();
-		render.startLoop();
+		
+		
 		
 	}
 }
--- src/render/renderer.java
+++ src/render/renderer.java
@@ -1,11 +1,9 @@
 package render;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.FloatBuffer;
-import java.nio.IntBuffer;
 
 import org.lwjgl.BufferUtils;
 import org.lwjgl.LWJGLException;
@@ -19,165 +17,214 @@ import org.lwjgl.opengl.GL30;
 import org.lwjgl.opengl.PixelFormat;
 import org.lwjgl.util.glu.GLU;
 
+
 public class renderer {
-	private  final int WIDTH = 800;
-	private  final int HEIGHT = 600;
-	private  final int fps = 60;
-	private int vaoId = 0;
-	private int vboId = 0;
-	private int vbocId = 0;
-	private int vboiId = 0;
-	private int iCount;
-	private static final String vertexFile = "src/shaders/VertexShader.glsl";
-	private static final String fragmentFile = "src/shaders/FragmentShader.glsl";
-	private int shaderProg = 0;
-	private int vShaderID;
-	private int fShaderID;
-
-	public void startLoop(){
+		private final int WIDTH = 800;
+		private final int HEIGHT = 600;
+		private int vaoId = 0;
+		private int vboId = 0;
+		private int vbocId = 0;
+		private int vboiId = 0;
+		private int indicesCount = 0;
+		private int vsId = 0;
+		private int fsId = 0;
+		private int shaderProg = 0;
 		
-	while(!Display.isCloseRequested()){
-		
-		
-		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
-		GL20.glUseProgram(shaderProg);
-		GL30.glBindVertexArray(vaoId);
-		GL20.glEnableVertexAttribArray(0);
-		GL20.glEnableVertexAttribArray(1);
-		GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, vboiId);
-		GL11.glDrawElements(GL11.GL_TRIANGLES, iCount, GL11.GL_UNSIGNED_INT, 0);
-		
-		GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, 0);
-		GL20.glDisableVertexAttribArray(0);
-		GL20.glDisableVertexAttribArray(1);
-		GL30.glBindVertexArray(0);
-		GL20.glUseProgram(0);
-		Display.sync(fps);
-		Display.update();
-	}
-	
-	Display.destroy();
-}
-public void initDisplay(){
-ContextAttribs attribs = new ContextAttribs(3,2).withForwardCompatible(true).withProfileCore(true);
-try {
-	Display.setDisplayMode(new DisplayMode(WIDTH,HEIGHT));
-	Display.create(new PixelFormat(), attribs);
-	//Display.create();
-	Display.setTitle("glTest");
-	GL11.glViewport(0,0,Display.getWidth(),Display.getHeight());
-} catch (LWJGLException e) {
-	e.printStackTrace();//do something more usefull here.
-	System.exit(-1);
-}
-GL11.glClearColor(0.6f,0.6f,0.6f,0);
-GL11.glViewport(0,0,Display.getWidth(),Display.getHeight());
-
-}
-
-public void intitShape(){
-float[] vertices = {
-		-0.5f,0.5f,0,1f
-	    -0.5f,-0.5f,0,1f,
-		0.5f,-0.5f,0,1f,
-		0.5f,0.5f,0,1f
-};
-
-FloatBuffer vBuffer = BufferUtils.createFloatBuffer(vertices.length);
-vBuffer.put(vertices);
-vBuffer.flip();//switch to read.
-
-float[] colors = {
-		1f, 0f, 0f, 1f,
-		0f, 1f, 0f, 1f,
-		0f, 0f, 1f, 1f,
-		1f, 1f, 1f, 1f,
-		};
-
-FloatBuffer cBuffer = BufferUtils.createFloatBuffer(colors.length);
-cBuffer.put(colors);
-cBuffer.flip();
-
-byte[] indices = {
-		0,1,2,
-		2,3,0
-};
-ByteBuffer iBuffer = BufferUtils.createByteBuffer(indices.length);
-iBuffer.put(indices);
-iBuffer.flip();
-iCount = indices.length;
-
-vaoId = GL30.glGenVertexArrays();
-GL30.glBindVertexArray(vaoId);
-
-vboId = GL15.glGenBuffers();
-GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vboId);
-GL15.glBufferData(GL15.GL_ARRAY_BUFFER, vBuffer, GL15.GL_STATIC_DRAW);
-GL20.glVertexAttribPointer(0, 4, GL11.GL_FLOAT, false, 0, 0);
-GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
-
-vbocId = GL15.glGenBuffers();
-GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vbocId);
-GL15.glBufferData(GL15.GL_ARRAY_BUFFER, cBuffer, GL15.GL_STATIC_DRAW);
-GL20.glVertexAttribPointer(1, 4, GL11.GL_FLOAT, false, 0, 0);
-GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
-
-GL30.glBindVertexArray(0);
-
-vboiId = GL15.glGenBuffers();
-GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, vboiId);
-GL15.glBufferData(GL15.GL_ELEMENT_ARRAY_BUFFER, iBuffer, GL15.GL_STATIC_DRAW);
-GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, 0);
-}
-
-public void intitShaders(){
-	int errorCheckValue = GL11.glGetError();
-	vShaderID = parseShaderFile(vertexFile, GL20.GL_VERTEX_SHADER);
-	fShaderID = parseShaderFile(fragmentFile, GL20.GL_FRAGMENT_SHADER);
-	shaderProg = GL20.glCreateProgram();
-	GL20.glAttachShader(shaderProg, vShaderID);
-	GL20.glAttachShader(shaderProg, fShaderID);
-	GL20.glBindAttribLocation(shaderProg, 0, "in_Position");
-	GL20.glBindAttribLocation(shaderProg, 1, "in_Color");
-	GL20.glLinkProgram(shaderProg);
-	GL20.glValidateProgram(shaderProg);
-	errorCheckValue = GL11.glGetError();
-	if (errorCheckValue != GL11.GL_NO_ERROR) {
-		System.out.println("ERROR - Could not create the shaders:" + GLU.gluErrorString(errorCheckValue));
-		System.exit(-1);
-	}
-}
-
-public int parseShaderFile(String file, int type){
-	StringBuilder sCode = new StringBuilder();
-	try {
-		BufferedReader reader = new BufferedReader(new FileReader(file));
-		String nextLine;
-		while((nextLine = reader.readLine())!=null){
-			sCode.append(nextLine).append("\n");
+		public renderer() {
+			
+			this.initDisplay();
+			this.initShape();
+			this.initShaders();
+			
+			System.out.println("Rendering With following parameters..");
+	        System.out.println("vaoId:"+vaoId);
+	        System.out.println("vboId:"+vboId);
+	        System.out.println("vbocId:"+ vbocId);
+	        System.out.println("vboiId:"+ vboiId);
+	        System.out.println("iCount:"+ indicesCount);
+	        System.out.println("shaderProg:"+ shaderProg);
+	        System.out.println("vShaderID:"+ vsId);
+	        System.out.println("fShaderID:"+ fsId);
+			while (!Display.isCloseRequested()) {
+				
+				GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
+				GL20.glUseProgram(shaderProg);
+				GL30.glBindVertexArray(vaoId);
+				GL20.glEnableVertexAttribArray(0);
+				GL20.glEnableVertexAttribArray(1);
+				GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, vboiId);
+				GL11.glDrawElements(GL11.GL_TRIANGLES, indicesCount, GL11.GL_UNSIGNED_BYTE, 0);
+				GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, 0);
+				GL20.glDisableVertexAttribArray(0);
+				GL20.glDisableVertexAttribArray(1);
+				GL30.glBindVertexArray(0);
+				GL20.glUseProgram(0);
+				Display.sync(60);
+				Display.update();
+			}
+			
+			
+			this.cleanUp();
 		}
-	reader.close();
-	
-	} 
-	catch (IOException e) {
-		e.printStackTrace();//TODO do something more usefull here
-	}
-	int shaderID = GL20.glCreateShader(type);
-	if(type == GL20.GL_FRAGMENT_SHADER){
-		System.out.println("Compiling Fragment Shader..");
-	}
-	else{
-		System.out.println("Compiling Vertex Shader..");
-	}
-	System.out.print(sCode);
-	GL20.glShaderSource(shaderID, sCode);
-	GL20.glCompileShader(shaderID);
-	if(GL20.glGetShaderi(shaderID,GL20.GL_COMPILE_STATUS)==GL11.GL_FALSE){
-		System.out.println(GL20.glGetShaderInfoLog(shaderID, 500));
-		System.exit(-1);
-	}
-	System.out.println("..Done.");
-	return shaderID;
-}
 
+		public void initDisplay() {
+			
+			try {
+				PixelFormat pixelFormat = new PixelFormat();
+				ContextAttribs contextAtrributes = new ContextAttribs(3, 2)
+					.withForwardCompatible(true)
+					.withProfileCore(true);
+				
+				Display.setDisplayMode(new DisplayMode(WIDTH, HEIGHT));
+				Display.setTitle("glTest");
+				Display.create(pixelFormat, contextAtrributes);
+				GL11.glViewport(0, 0, WIDTH, HEIGHT);
+			} catch (LWJGLException e) {
+				e.printStackTrace();
+				System.exit(-1);
+			}
+			GL11.glClearColor(0.6f, 0.6f, 0.6f, 0f);
+			GL11.glViewport(0, 0, WIDTH, HEIGHT);
+		}
+		
+		public void initShape() {
+			
+			float[] vertices = {
+					-0.5f, 0.5f, 0f, 1f,
+					-0.5f, -0.5f, 0f, 1f,
+					0.5f, -0.5f, 0f, 1f,
+					0.5f, 0.5f, 0f, 1f
+			};
+			FloatBuffer verticesBuffer = BufferUtils.createFloatBuffer(vertices.length);
+			verticesBuffer.put(vertices);
+			verticesBuffer.flip();
+			
+			float[] colors = {
+					0.2f, 0.4f, 0.6f, 0f,//top right
+					0f, 0f, 0f, 0f,
+					0f, 0f, 0f, 0f,
+					0.2f, 0.4f, 0.6f, 0f,//top left
+			};
+			FloatBuffer colorsBuffer = BufferUtils.createFloatBuffer(colors.length);
+			colorsBuffer.put(colors);
+			colorsBuffer.flip();
+			byte[] indices = {
+					0, 1, 2,
+					2, 3, 0
+			};
+			indicesCount = indices.length;
+			ByteBuffer indicesBuffer = BufferUtils.createByteBuffer(indicesCount);
+			indicesBuffer.put(indices);
+			indicesBuffer.flip();
+			vaoId = GL30.glGenVertexArrays();
+			GL30.glBindVertexArray(vaoId);
+			
+			vboId = GL15.glGenBuffers();
+			GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vboId);
+			GL15.glBufferData(GL15.GL_ARRAY_BUFFER, verticesBuffer, GL15.GL_STATIC_DRAW);
+			GL20.glVertexAttribPointer(0, 4, GL11.GL_FLOAT, false, 0, 0);
+			GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
+			
+			vbocId = GL15.glGenBuffers();
+			GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vbocId);
+			GL15.glBufferData(GL15.GL_ARRAY_BUFFER, colorsBuffer, GL15.GL_STATIC_DRAW);
+			GL20.glVertexAttribPointer(1, 4, GL11.GL_FLOAT, false, 0, 0);
+			GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
+			
+			
+			GL30.glBindVertexArray(0);
+			
+			
+			vboiId = GL15.glGenBuffers();
+			GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, vboiId);
+			GL15.glBufferData(GL15.GL_ELEMENT_ARRAY_BUFFER, indicesBuffer, GL15.GL_STATIC_DRAW);
+			GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, 0);
+		}
+		
+		private void initShaders() {
+			int errorCheckValue = GL11.glGetError();
+			vsId = this.loadShaderData("src/shaders/VertexShader.glsl", GL20.GL_VERTEX_SHADER);
+			fsId = this.loadShaderData("src/shaders/FragmentShader.glsl", GL20.GL_FRAGMENT_SHADER);
+			
+			shaderProg = GL20.glCreateProgram();
+			GL20.glAttachShader(shaderProg, vsId);
+			GL20.glAttachShader(shaderProg, fsId);
+			GL20.glBindAttribLocation(shaderProg, 0, "in_Position");
+			GL20.glBindAttribLocation(shaderProg, 1, "in_Color");
+			GL20.glLinkProgram(shaderProg);
+			GL20.glValidateProgram(shaderProg);
+			
+			errorCheckValue = GL11.glGetError();
+			if (errorCheckValue != GL11.GL_NO_ERROR) {
+				System.out.println("ERROR - Could not create the shaders:" + GLU.gluErrorString(errorCheckValue));
+				System.exit(-1);
+			}
+		}
+		public void cleanUp() {
+			// Delete the shaders
+			GL20.glUseProgram(0);
+			GL20.glDetachShader(shaderProg, vsId);
+			GL20.glDetachShader(shaderProg, fsId);
+			
+			GL20.glDeleteShader(vsId);
+			GL20.glDeleteShader(fsId);
+			GL20.glDeleteProgram(shaderProg);
+			
+			// Select the VAO
+			GL30.glBindVertexArray(vaoId);
+			
+			// Disable the VBO index from the VAO attributes list
+			GL20.glDisableVertexAttribArray(0);
+			GL20.glDisableVertexAttribArray(1);
+			
+			// Delete the vertex VBO
+			GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
+			GL15.glDeleteBuffers(vboId);
+			
+			// Delete the color VBO
+			GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
+			GL15.glDeleteBuffers(vbocId);
+			
+			// Delete the index VBO
+			GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, 0);
+			GL15.glDeleteBuffers(vboiId);
+			
+			// Delete the VAO
+			GL30.glBindVertexArray(0);
+			GL30.glDeleteVertexArrays(vaoId);
+			
+			Display.destroy();
+		}
+		
+		public int loadShaderData(String file, int type) {
+			StringBuilder sCode = new StringBuilder();
+		    try {
+		        BufferedReader reader = new BufferedReader(new FileReader(file));
+		        String nextLine;
+		        while((nextLine = reader.readLine())!=null){
+		            sCode.append(nextLine).append("\n");
+		        }
+		    reader.close();
+
+		    }
+		    catch (IOException e) {
+		        e.printStackTrace();//TODO do something more usefull here
+		    }
+		    int shaderID = GL20.glCreateShader(type);
+		    if(type == GL20.GL_FRAGMENT_SHADER){
+		        System.out.println("Compiling Fragment Shader..");
+		    }
+		    else{
+		        System.out.println("Compiling Vertex Shader..");
+		    }
+		    System.out.print(sCode);
+		    GL20.glShaderSource(shaderID, sCode);
+		    GL20.glCompileShader(shaderID);
+		    if(GL20.glGetShaderi(shaderID,GL20.GL_COMPILE_STATUS)==GL11.GL_FALSE){
+		        System.out.println(GL20.glGetShaderInfoLog(shaderID, 500));
+		        System.exit(-1);
+		    }
+		    System.out.println("..Done.");
+		    return shaderID;
+		}
 }
